aea90fab16620169e5bef22f1f9042b89d8cd755
Jonathan Linczak <jlinczak@icloud.com>
2022-01-19 12:18:09 -0500

    A stupid example without any fun logic for dealing with POST as a controller on the backend

--- demo/src/main/java/com/devopsdemo/demo/controller/FileUploadController.java
+++ demo/src/main/java/com/devopsdemo/demo/controller/FileUploadController.java
@@ -1,4 +1,6 @@
-package com.devopsdemo.demo;
+package com.devopsdemo.demo.controller;
+
+import java.io.IOException;
 
 /***
  * This was taken from Spring Boot's Uploading Files Guide located
@@ -7,19 +9,15 @@ package com.devopsdemo.demo;
  */
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FileUploadController {
 
-  @PostMapping("/")
-  public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
-    /* This is where we would actually do the post and retrieve logic */
-    redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + file.getOriginalFilename() + "!");
-    return "redirect:/";
-  }
+	@GetMapping("/")
+	public String listUploadedFiles(Model model) throws IOException {
+		return "uploadForm";
+	}
 
 }
